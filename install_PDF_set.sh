ce9df9ad141dabf1b073c9787cb681d09bdd6e79
muhammadgul <mgul@cern.ch>
2019-10-31 15:31:38 +0100

    Update install_PDF_set.sh

--- install_PDF_set.sh
+++ install_PDF_set.sh
@@ -3,7 +3,7 @@
 cd share/LHAPDF/
 
 LHAPDF_version=6.1.6
-PDFs="NNPDF30_lo_as_0130 NNPDF30_lo_as_0130_nf_4 NNPDF30_lo_as_0118 NNPDF23_lo_as_0130_qed NNPDF23_lo_as_0119_qed cteq6l1 MMHT2014lo68cl MMHT2014lo_asmzsmallrange HERAPDF15LO_EIG NNPDF30_nlo_as_0118 NNPDF23_nlo_as_0119 CT10nlo MMHT2014nlo68cl"
+PDFs="NNPDF30_lo_as_0118 NNPDF30_nlo_as_0118 NNPDF30_nnlo_as_0118"
 
 for PDF in $PDFs; do
   echo "Installing $PDF PDF set"
